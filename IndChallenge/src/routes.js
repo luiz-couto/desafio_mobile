d6a4ee4bb47a4399ecf532180f3513ff52811e55
luiz-couto <luiz.felipe.556@outlook.com>
2019-03-31 00:31:40 -0300

    Set the menu background

--- IndChallenge/src/routes.js
+++ IndChallenge/src/routes.js
@@ -1,18 +1,50 @@
-import { createStackNavigator, createAppContainer,createDrawerNavigator } from 'react-navigation';
+import { createStackNavigator, createAppContainer,createDrawerNavigator,DrawerItems} from 'react-navigation';
+import React from 'react'
+import { View, Text, ImageBackground, StyleSheet} from 'react-native'
 import Main from './pages/main/index';
 //import Data from './pages/data/index';
 
 const RootStack = createDrawerNavigator({
-  
+
   Main: { screen: Main },
   //Data: { screen: Data },
 }, {
 
-  drawerWidth: 250,
-  drawerPosition: 'left',
+  contentComponent: (props) => (
+
+        <View>
+
+          <ImageBackground
+
+          source={require('./background.png')}
+          style={styles.background}
+          resizeMode = 'cover'
+
+         />
+
+          <Text>Custom Header</Text>
+          <DrawerItems {...props} />
+          <Text>Custom Footer</Text>
+        </View>
+      )
+
+
 
 });
+
+const styles = StyleSheet.create({
+
+
+  background: {
+
+    width:280,
+    height:740,
+    position:'absolute',
+
+  },
+})
 
 const Routes = createAppContainer(RootStack)
 
 export default Routes;
+

75c303da837054d69125ab3b984c322dbf0ea24d
luiz-couto <luiz.felipe.556@outlook.com>
2019-04-12 21:25:11 -0300

    Styling the addComment page

--- IndChallenge/src/pages/data/addComment/index.js
+++ IndChallenge/src/pages/data/addComment/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {View,Image,TextInput} from 'react-native'
+import {View,Image,TextInput,Text,TouchableOpacity} from 'react-native'
 
 import Header from './header'
 import styles from './styles';
@@ -8,14 +8,82 @@ import styles from './styles';
 export default class AddComment extends React.Component{
 
 
+    constructor(props){
+        super(props);
+        this.state = {
+            
+            title: '',
+            text: '',
+            
+          
+        }
+    }
+
     render(){
         
+        
+
+
         const{ navigation } = this.props;
 
         return(
 
             <View style={styles.container}>
+                
                 <Header navigation={navigation} />
+
+            <View styles={styles.title_container}>
+
+                <View style={styles.title_text_container}>
+                    <Text style={styles.title_text}>Título</Text>
+                </View>
+               
+                <TextInput
+                    style={styles.input_title}
+                    onChangeText={(title) => this.setState({title})}
+                    value={this.state.title}
+                    placeholder={'Escreva aqui o título'}
+                    placeholderTextColor={'rgba(255,255,255,0.2)'}
+                    underlineColorAndroid={'transparent'}
+                    autoCorrect={false}
+                    
+                />
+
+                <View style={styles.comment_text_container}>
+                    <Text style={styles.comment_text}>Descrição</Text>
+                </View>
+               
+                <TextInput
+                    style={styles.input_comment}
+                    onChangeText={(text) => this.setState({text})}
+                    value={this.state.text}
+                    underlineColorAndroid={'transparent'}
+                    multiline = {true}
+                    autoCorrect={false}
+                />
+
+
+                <View style={styles.buttons_container}>
+                    <View>
+                        <TouchableOpacity onPress={() => {this.props.navigation.navigate('Data')}} style={styles.back_touchable}>
+                            <View style={styles.back_button}>
+                            <Text style={styles.back_text}>Cancelar</Text>
+                            </View>
+                        </TouchableOpacity>
+                    </View>
+
+                    <View>
+                        <TouchableOpacity onPress={() => {}} style={styles.publish_touchable}>
+                            <View style={styles.publish_button}>
+                            <Text style={styles.publish_text}>Pronto!</Text>
+                            </View>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+            </View>
+
+
+
             </View>
 
 
--- IndChallenge/src/pages/data/addComment/styles.js
+++ IndChallenge/src/pages/data/addComment/styles.js
@@ -13,6 +13,138 @@ const styles = StyleSheet.create({
 
    },
 
+   input_title:{
+
+    width: 330,
+    height: 40,
+    borderColor: 'rgba(255,255,255,0.6)',
+    borderWidth: 1,
+    marginLeft: 15,
+    marginTop: 5,
+    borderRadius: 4,
+    backgroundColor:'rgb(255,255,255)',
+    fontFamily:'Manjari-Thin',
+    
+    
+
+
+   },
+
+   title_text_container:{
+
+    marginLeft: 18,
+    marginTop: 30,
+
+   },
+
+   title_text:{
+
+    fontSize: 18,
+    fontFamily:'Manjari-Thin',
+    color: 'rgb(255,255,255)',
+    
+
+   },
+
+   input_comment:{
+    
+    width: 330,
+    height: 300,
+    borderColor: 'rgba(255,255,255,0.6)',
+    borderWidth: 1,
+    marginLeft: 15,
+    marginTop: 5,
+    backgroundColor:'rgb(255,255,255)',
+    borderRadius: 4,
+    textAlignVertical: 'top',
+    fontFamily:'Manjari-Thin',
+    fontSize: 18,
+
+   },
+
+   comment_text:{
+
+    fontSize: 18,
+    fontFamily:'Manjari-Thin',
+    color: 'rgb(255,255,255)',
+
+   },
+
+   comment_text_container:{
+
+    marginLeft: 18,
+    marginTop: 25,
+   
+   },
+
+back_button:{
+
+    borderWidth:0.4,
+    borderColor: 'white',
+    paddingLeft:wp('5.33%'),
+    paddingRight:wp('5.33%'),
+    paddingTop:8,
+    paddingBottom: hp('0.13%'),
+    borderRadius: 3,
+    height:hp('6.33%'),
+    width: wp('43%'),
+    alignItems:'center'
+
+
+},
+
+back_text:{
+
+    fontFamily:'Manjari-Thin',
+    fontSize:hp('3.0%'),
+    color:'rgb(255,255,255)',
+
+},
+
+back_touchable:{
+
+    marginLeft:16,
+    marginTop:20
+},
+
+publish_button:{
+
+    borderWidth:0.4,
+    borderColor: 'white',
+    paddingLeft:wp('5.33%'),
+    paddingRight:wp('5.33%'),
+    paddingTop:8,
+    paddingBottom: hp('0.13%'),
+    borderRadius: 3,
+    height:hp('6.33%'),
+    width: wp('43%'),
+    alignItems:'center',
+    backgroundColor:'rgb(255,255,255)'
+
+
+},
+
+publish_text:{
+
+    fontFamily:'Manjari-Thin',
+    fontSize:hp('3.0%'),
+    color:'rgb(155,155,155)',
+
+},
+
+publish_touchable:{
+
+    marginLeft:18,
+    marginTop:20
+},
+
+
+
+buttons_container:{
+
+    flexDirection:'row',
+
+},
   
 });
 
